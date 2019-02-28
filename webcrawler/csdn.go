09f40f472c33316193382f1c7c5a672dc7356de5
徐海瀚 <xhhscau2015@163.com>
2019-02-28 22:53:53 +0800

    增加代理访问，防止禁ip

--- webcrawler/csdn.go
+++ webcrawler/csdn.go
@@ -4,60 +4,63 @@ import (
 	"github.com/axgle/mahonia"
 	"github.com/PuerkitoBio/goquery"
 	"strconv"
-	"net/http"
 	"fmt"
+	"../utils"
+	"io/ioutil"
 )
+
 type Crawler interface {
-	IncreaseVisits(blog string,data []string)
-	GetArticles(url string,num int) []string
+	IncreaseVisits(blog string, data []string)
+	GetArticles(url string, num int) []string
 }
 
-
-func NewCsdn() Crawler{
+func NewCsdn() Crawler {
 	return &csdn{}
 }
 
 type csdn struct {
-
 }
 
-func(c *csdn) IncreaseVisits(blog string,data []string){
-	if data ==nil{
+func (c *csdn) IncreaseVisits(blog string, data []string) {
+	if data == nil {
 		return
 	}
 	/*header:=make(map[string]string)
 	header["Referer"]="https://blog.csdn.net/"+blog
 	header["User-Agent"]="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
 	*/
-	for _,value:=range data{
-        	_,err:=http.Get(value)
-        	if err!=nil{
-        		fmt.Println(err.Error())
-			}
+	ip := utils.ReturnIp()
+	for _, value := range data {
+		res := utils.GetRep(value, ip)
+		if res.StatusCode != 200 {
+			fmt.Println(res.StatusCode)
+		}
 	}
 }
 
-func(* csdn) GetArticles(url string,num int) []string{
+func (*csdn) GetArticles(url string, num int) []string {
 	var i int
 	var data []string
-	if num<0{
+	if num < 0 {
 		return nil
 	}
-	for i=1;i<=num;i++{
-		newUrl:=url+strconv.Itoa(i)
-		res,_:=http.Get(newUrl)
+	for i = 1; i <= num; i++ {
+		newUrl := url + strconv.Itoa(i)
+		//res,_:=http.Get(newUrl)
+		ip := utils.ReturnIp()
+		res := utils.GetRep(newUrl, ip)
+		body, _ := ioutil.ReadAll(res.Body)
+		fmt.Println(string(body))
 		dec := mahonia.NewDecoder("utf-8")
 		doc := dec.NewReader(res.Body)
 		result, _ := goquery.NewDocumentFromReader(doc)
 		result.Find(".content").Each(func(i int, selection *goquery.Selection) {
-			if i<=0 {
+			if i <= 0 {
 				return
 			}
-			v,_:=selection.Find("a").Attr("href")
-			data=append(data,v)
+			v, _ := selection.Find("a").Attr("href")
+			data = append(data, v)
 		})
 	}
 	return data
 }
-
-
